45abb501bdfe2309664743da4bee54d55521ff23
Chanya Punyakumpol <chanya.punyakumpol@graduateinstitute.ch>
2022-04-20 00:26:22 -0500

    Intro to R Script

---
+++ Intro_R_Script.R
@@ -0,0 +1,14 @@
+#Introduction to R Workshop Script
+#This is the R Script for the introduction to R workshop organized by the
+#Created by Chanya Punyakumpol, 2022
+
+#Basic arithmetic
+2+3
+4*5-8
+(6/3)+5
+2^3
+5%%2
+
+#
+
+
